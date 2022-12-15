e9206b90e041c9a0c320404c83d3cac5557bc26a
Kaci Wooldridge <kaci.school.info@gmail.com>
2022-12-15 10:31:39 -0600

    updated post list

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -4,7 +4,6 @@ import { postList } from "./feed/PostList.js"
 export const GiffyGram = () => {
 
     return `<h1>Giffygram</h1>
-            ${Footer()}
-
-    ${postList()}`
+            ${postList()}
+            ${Footer()}`
 }
