ec445b2c1c7ab1513d7089f28a0043c2644a66fe
Chris Hanson <chanson8@gmail.com>
2022-12-15 10:47:59 -0600

    add new post functionality

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,26 +1,61 @@
-const apiURL = "http://localhost:8088"
-const applicationElement = document.querySelector(".giffygram")
-
+const apiURL = "http://localhost:8088";
+const applicationElement = document.querySelector(".giffygram");
 
 const applicationState = {
-    currentUser: {},
-    feed: {
-        chosenUser: null,
-        displayFavorites: false,
-        displayMessages: false
-    }
-}
+  currentUser: {},
+  feed: {
+    chosenUser: null,
+    displayFavorites: false,
+    displayMessages: false,
+    displayPostEntry: true,
+  },
+};
 
 export const fetchUsers = () => {
-    return fetch(`${apiURL}/users`)
-        .then(response => response.json())
-        .then(
-            (data) => {
-                applicationState.users = data
-            }
-        )
+  return fetch(`${apiURL}/users`)
+    .then((response) => response.json())
+    .then((data) => {
+      applicationState.users = data;
+    });
+};
+
+export const fetchPosts = () => {
+  return fetch(`${apiURL}/posts`)
+  .then((response) => response.json())
+  .then((data) => {
+    applicationState.posts = data
+  })
 }
 
+export const getPostEntryStatus = () => {
+  return applicationState.feed.displayPostEntry;
+};
+
 export const getUsers = () => {
-    return applicationState.users.map(user => ({...user}))
-}
+  return applicationState.users.map((user) => ({ ...user }));
+};
+
+export const getPosts = () => {
+  return applicationState.posts.map((p) => ({ ...p }));
+};
+
+export const setPostEntryStatus = (input) => {
+  applicationState.feed.displayPostEntry = input;
+  applicationElement.dispatchEvent(new CustomEvent("stateChanged"));
+};
+
+export const sendPostEntry = (postObj) => {
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postObj),
+  };
+
+  return fetch(`${apiURL}/posts`, fetchOptions)
+    .then((response) => response.json())
+    .then(() => {
+      applicationElement.dispatchEvent(new CustomEvent("stateChanged"));
+    });
+};
