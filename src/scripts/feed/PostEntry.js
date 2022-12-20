83d4170c7636aaf387ec7e0aebb0d4b8967d16d5
Chris Hanson <chanson8@gmail.com>
2022-12-20 12:50:11 -0600

    can now edit posts

--- src/scripts/feed/PostEntry.js
+++ src/scripts/feed/PostEntry.js
@@ -1,4 +1,4 @@
-import { getPostEntryStatus, setPostEntryStatus, sendPostEntry, getCurrentUser } from "../data/provider.js"
+import { getPostEntryStatus, setPostEntryStatus, sendPostEntry, getCurrentUser, getPosts, updatePost } from "../data/provider.js"
 
 export const postEntryForm = () => {
   if (!getPostEntryStatus()) {
@@ -8,6 +8,7 @@ export const postEntryForm = () => {
   } else {
     return `
     <div class="newPost">
+    <h2>Create a new post</h2>
     <div>
       <input value name="postTitle" class="newPost__input" type="text" placeholder="Title">
     </div>
@@ -22,9 +23,35 @@ export const postEntryForm = () => {
   }
 }
 
+export const editPostForm = (postId) => {
+  let posts = getPosts()
+  let foundPost = posts.find((post) => post.id === postId)
+  return `
+  <div class="edit__post">
+    <h2>Edit Your Post</h2>
+    <div>
+      <input name="edit_postTitle" class="newPost__input" type="text" value="${foundPost.postTitle}">
+    </div>
+    <div>
+      <input name="edit_postURL" class="newPost__input" type="text" value="${foundPost.postURL}">
+    </div>
+    <textarea name="edit_postDescription" class="newPost_input newPost__description">${foundPost.postDescription}</textarea>
+    <button id="editPost__submit--${postId}">Post</button>
+    <button id="edit_post_cancel">Cancel</button>
+  </div>
+  `
+}
+
 document.addEventListener("click", (event) => {
   if (event.target.id === "newPost__cancel") {
     setPostEntryStatus(false)
+  }
+})
+
+document.addEventListener("click", (event) => {
+  if (event.target.id === "edit_post_cancel") {
+    document.getElementById('edit_PostForm').innerHTML = ""
+    document.getElementById('edit_PostForm').style.border = ""
   }
 })
 
@@ -48,5 +75,24 @@ document.addEventListener("click", (event) => {
       date: new Date()
     }
     sendPostEntry(postInformation)
+  }
+})
+
+document.addEventListener("click", (event) => {
+  if (event.target.id.startsWith("editPost__submit")) {
+    let [, postId] = event.target.id.split("--")
+    postId = parseInt(postId)
+    const postTitle = document.querySelector("input[name='edit_postTitle']").value
+    const postURL = document.querySelector("input[name='edit_postURL']").value
+    const postDescription = document.querySelector("textarea[name='edit_postDescription']").value
+    const currentUser = getCurrentUser();
+    const postInformation = {
+      postTitle: postTitle,
+      postURL: postURL,
+      postDescription: postDescription,
+      userId: currentUser.id,
+      date: new Date()
+    }
+    updatePost(postInformation, postId)
   }
 })
